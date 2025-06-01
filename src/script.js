1e92a3b3ca175e1f3f2a55a4fd626901fa8e07ac
SibuleleG <gxidolosbulele@gmail.com>
2025-06-01 16:54:05 +0200

    added time zone and Id in html

--- src/script.js
+++ src/script.js
@@ -17,5 +17,28 @@ function updateTime() {
   accraDateElement.innerHTML = accraTime.format("MMMM Do YYYY");
   accraTimeElement.innerHTML = accraTime.format("h:mm:ss [<small>]A[</small>]");
 }
-updateTime(); // initial call
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (!cityTimeZone) return;
+
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
+    </div>`;
+}
+
+updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelect = document.querySelector("#city");
+citiesSelect.addEventListener("change", updateCity);
